caa78f59a6bd48be8432896aab3c45f618d8d922
CZUi <chenclch@outlook.com>
2024-03-11 23:52:46 +0800

    Basic BinaryTree and improvement of KMP

--- DataStructure_Algorithm/String/kmp.cpp
+++ DataStructure_Algorithm/String/kmp.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "String.hpp"
 
+typedef void (*pnext) (String, int*);
+
 void showArr(int *arr, int size)
 {
     ++arr;
@@ -16,26 +18,12 @@ void showArr(int *arr, int size)
 // 与后面的声明参数要一致
 void get_next(String T, int *next);
 
+void get_nextVal(String T, int *next);
+
 int index_KMP(String S, String T, int pos);
 
 int main()
 {
-    // // some test
-    // std::string x = "abbab";
-    // std::cout << x << std::endl;
-    // std::cout << x.size() << std::endl;
-    // std::cout << sizeof(x) << std::endl;
-    // const char *y = "abcaca";
-    // // available
-    // x = y;
-    // printf("%s,%s\n", x, y);
-    // std::cout << "x = " << x << " y = " << y << std::endl;
-    // std::string z(x);
-    // const char *u = y;
-    // // error
-    // // u = z;
-    // // start here
-    // std::cout << x[1] << std::endl;
     std::cout << "next of T is\n";
     const char *t = "ababa";
     String T(t);
@@ -72,12 +60,42 @@ void get_next(String T, int *next)
     }
 }
 
+// If T = "aaaaax" and S = "aaaaax", we have a better get_next!
+void get_nextVal(String T, int *nextVal)
+{
+    int i = 1;
+    int j = 0;
+    nextVal[1] = 0;
+    while (i < T.getSize())
+    {
+        if (j == 0 || T[i] == T[j])
+        {
+            ++i;
+            ++j;
+            // 当前字符与前缀字符不同，正常操作，同get_next
+            if (T[i] != T[j])
+                nextVal[i] = j;
+            // 当前字符与前缀字符相同，将前缀字符的nextval值赋给当前位置
+            else
+                nextVal[i] = nextVal[j];
+        }
+        else
+            j = nextVal[j];
+    }
+}
+
 int index_KMP(String S, String T, int pos)
 {
+    pnext Farr[2] = {get_next, get_nextVal};
+    std::cout << "Select get_next()...(1) get_next (2) get_nextVal\n";
+    int fun;
+    std::cin >> fun;
+    if (fun != 2) fun = 1;
+
     int i = pos;
     int j = 1;
     int next[255];
-    get_next(T, next);
+    Farr[fun - 1](T, next);
     while (i <= S.getSize() && j <= T.getSize())
     {
         if (j == 0 || S[i] == T[j])
--- DataStructure_Algorithm/Tree/BinaryTree.cpp
+++ DataStructure_Algorithm/Tree/BinaryTree.cpp
@@ -2,7 +2,7 @@
 
 struct TreeNode
 {
-    int value;
+    int value = -1;
     TreeNode *left;
     TreeNode *right;
 
@@ -13,13 +13,98 @@ class BinaryTree
 {
 public:
     BinaryTree() : root(nullptr) {}
+    ~BinaryTree() { DistroyBiTree(root); }
+
+    void CreateBiTree() { CreateBiTree(root); };
+    void PreOrderTraverse() { PreOrderTraverse(root); }
+    void InOrderTraverse() { InOrderTraverse(root); }
+    void PostOrderTraverse(){ PostOrderTraverse(root); }
 
 private:
+    // void CreateBiTree(TreeNode* T); THIS IS WRONG!!!!!
+    // You have to pass a reference of a pointer to the function
+    // Or it will just create a copy of the poiter
+    void CreateBiTree(TreeNode* &T);
+    void DistroyBiTree(TreeNode* &T);
+    void PreOrderTraverse(TreeNode *T);
+    void InOrderTraverse(TreeNode *T);
+    void PostOrderTraverse(TreeNode *T);
     TreeNode *root;
 };
 
+void BinaryTree::DistroyBiTree(TreeNode* &T)
+{
+    if (!T->left && !T->right)
+        return;
+    
+    DistroyBiTree(T->left);
+    DistroyBiTree(T->right);
+    if (T->left)
+    {
+        delete T->left;
+        T->left = nullptr;
+    }
+    if (T->right)
+    {
+        delete T->right;
+        T->right = nullptr;
+    }
+}
+
+void BinaryTree::PreOrderTraverse(TreeNode* T)
+{
+    if (!T)
+        return;
+
+    std::cout << T->value << " ";
+    PreOrderTraverse(T->left);
+    PreOrderTraverse(T->right);
+}
+
+void BinaryTree::InOrderTraverse(TreeNode *T)
+{
+    if (!T)
+        return;
+    
+    InOrderTraverse(T->left);
+    std::cout << T->value << " ";
+    InOrderTraverse(T->right);
+}
+
+void BinaryTree::PostOrderTraverse(TreeNode *T)
+{
+    if (!T)
+        return;
+    
+    PostOrderTraverse(T->left);
+    PostOrderTraverse(T->right);
+    std::cout << T->value << " ";
+}
+
+void BinaryTree::CreateBiTree(TreeNode* &T)
+{
+    int data;
+    std::cout << "Enter any value...(0 to as a leaf)\n";
+    std::cin >> data;
+    T = new TreeNode(data);
+    if (T->value == 0)
+        return;
+    
+    CreateBiTree(T->left);
+    CreateBiTree(T->right);
+}
+
 int main()
 {
-    BinaryTree tree;
+    BinaryTree* pt = new BinaryTree();
+    pt->CreateBiTree();
+    pt->PreOrderTraverse();
+    std::cout << '\n';
+    pt->InOrderTraverse();
+    std::cout << '\n';
+    pt->PostOrderTraverse();
+    std::cout << '\n';
+    delete pt;
+    pt = nullptr;
     return 0;
 }
